4381f02bf08df2c06937b61fffd86a4095afae83
Davis Justin Morris <davis@ibosoninnov.com>
2021-10-10 17:29:55 +0530

    demo display custom 3d by pk
    updated art section art details
    
    done dynamically through js
    
    (cherry picked from commit f153b43d053e8b570ce926c017c4c2fc7a04b70f)
    
    # Conflicts:
    #	public/js/home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -41,10 +41,11 @@ $(document).ready(function () {
     $(`#portfolio > section .frame`).on('click', function () {
         let data = res_map[this.id];
         $(`body > .image_view img`).remove();
+        $(`body > .image_view > .im_container > div`).remove();
 
         $(`body > .image_view > .im_container`).append(`
-<div style="width: 90vw; height: 95vh;">
-    <div style="left: 0px; width: 100%; height: 0px; position: relative; padding-bottom: 62.5%; overflow: hidden;">
+<div>
+    <div>
         <iframe src="test_XR.58.html"
                 allowfullscreen
                 style="position: absolute; top: 0px; left: 0px; height: 100%; width: 1px; min-width: 100%; *width: 100%;"
